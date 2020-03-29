826bfc25a2c9ea98fc34fd75f524fc9d7478489f
foyjulislam <foyjulislam.syl20@gmail.com>
2020-03-29 11:47:32 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAiQF7AfAgtLCsBet5gtvM_4Rr3rk8cWDM",
+    authDomain: "ema-jhon-easy.firebaseapp.com",
+    databaseURL: "https://ema-jhon-easy.firebaseio.com",
+    projectId: "ema-jhon-easy",
+    storageBucket: "ema-jhon-easy.appspot.com",
+    messagingSenderId: "643957630497",
+    appId: "1:643957630497:web:988273d91d0dbdf8bf6560"
+  };
+
+  export default firebaseConfig;
